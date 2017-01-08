09ca07ec50b68bc259ad5c7dd0a88965ec717510
Bec507 <beccaa507@googlemail.com>
2017-01-08 15:46:51 +0000

    4.14 added pagination

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
-  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_desc, -> { order(rating: :desc) }   
+  validates :body, presence: true
+    validates :user, presence: true
+    validates :product, presence: true
+    validates :rating, numericality: { only_integer: true }
 end
